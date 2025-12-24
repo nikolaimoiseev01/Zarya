55ef61ad93b918afc4593cbba6154febe88ac266
nikolaimoiseev01 <89159295+nikolaimoiseev01@users.noreply.github.com>
2025-12-24 17:45:00 +0300

    Initial commit

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,5 @@
+<a href="/" wire:navigate class="w-fit">
+    <img src="/fixed/logo.png"
+         {{ $attributes->merge(['class' => '']) }}
+         alt="logo">
+</a>
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Lato', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                main_bg: '#ECEBF3',
+                maroon: {
+                    500: '#ECEBF3'
+                }
+            },
+            screens: {
+                '2xl': {'max': '1535px'}, // => @media (max-width: 1535px) { ... }
+                'xl': {'max': '1279px'}, // => @media (max-width: 1279px) { ... }
+                'lg': {'max': '1023px'}, // => @media (max-width: 1023px) { ... }
+                'md': {'max': '767px'}, // => @media (max-width: 767px) { ... }
+                'sm': {'max': '639px'}, // => @media (max-width: 639px) { ... }
+            },
+        },
+    },
+    plugins: [forms],
+};
