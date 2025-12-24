06db85607dc86f63df4dea561433cd59e88aa790
Nikolai <nikolaimoiseev01@gmail.com>
2025-12-24 20:36:02 +0300

    Minor

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -7,6 +7,13 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <link rel="icon" type="image/png" href="/fixed/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/fixed/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="/fixed/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/fixed/favicon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+    <link rel="manifest" href="/fixed/favicon/site.webmanifest" />
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
