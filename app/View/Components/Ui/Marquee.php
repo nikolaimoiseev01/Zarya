a922c87b3970d68e5b039c47808e91e6fac9dd3f
Nikolai <nikolaimoiseev01@gmail.com>
2026-02-09 22:44:18 +0300

    Minor

--- app/View/Components/Ui/Marquee.php
+++ app/View/Components/Ui/Marquee.php
@@ -24,6 +24,6 @@ class Marquee extends Component
 
     public function render(): View|Closure|string
     {
-        return view('components.Ui.marquee');
+        return view('components.ui.marquee');
     }
 }
